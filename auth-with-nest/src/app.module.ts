fdd526cd383cc4790815de9da5071c8f5e3b9eb6
Alexandr Siryj <theobroma222@gmail.com>
2022-08-15 00:46:30 +0300

    users schema and service

--- auth-with-nest/src/app.module.ts
+++ auth-with-nest/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb://localhost/authentication'), UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ auth-with-nest/src/users/users.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { UsersService } from './user.service';
+import { UsersController } from './user.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserSchema } from './user.model';
+import { UsersService } from './users.service';
+
+@Module({
+  imports: [MongooseModule.forFeature([{ name: 'user', schema: UserSchema }])],
+  providers: [UsersService],
+  controllers: [UsersController],
+})
+export class UserModule {}
